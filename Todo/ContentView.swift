8fb1ba17f7f1b32321c05c75bbe931c95468a661
Rophy Tsai <rophyt@amazon.com>
2021-01-04 03:22:54 +0800

    example code for amplify

--- Todo/ContentView.swift
+++ Todo/ContentView.swift
@@ -1,16 +1,46 @@
-//
-//  ContentView.swift
-//  Todo
-//
-//  Created by Tsai, Yi-lin on 2021/1/4.
-//
-
 import SwiftUI
+import Amplify
+import AmplifyPlugins
+
+struct TodoRow: View {
+    var todo: TodoItem
+    
+    var body: some View {
+        Text(todo.name)
+    }
+}
+
+struct TodoItem: Identifiable {
+    var id = UUID()
+    var name: String
+}
 
 struct ContentView: View {
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        var todoRows = [] as [TodoItem]
+        Amplify.DataStore.query(Todo.self) { result in
+            switch(result) {
+            case .success(let todos):
+                for todo in todos {
+                    todoRows.append(TodoItem(name: todo.name))
+                    print("==== Todo ====")
+                    print("Name: \(todo.name)")
+                    if let priority = todo.priority {
+                        print("Priority: \(priority)")
+                    }
+                    if let description = todo.description {
+                        print("Description: \(description)")
+                    }
+                }
+            case .failure(let error):
+                print("Could not query DataStore: \(error)")
+            }
+        }
+        return List(todoRows) { todoRow in
+            TodoRow(todo: todoRow)
+            
+        }
     }
 }
 
--- Todo/TodoApp.swift
+++ Todo/TodoApp.swift
@@ -6,12 +6,33 @@
 //
 
 import SwiftUI
+import Amplify
+import AmplifyPlugins
 
 @main
 struct TodoApp: App {
+
+    // add a default initializer and configure Amplify
+    public init() {
+        configureAmplify()
+    }
+
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+func configureAmplify() {
+    Amplify.Logging.logLevel = .info
+    let dataStorePlugin = AWSDataStorePlugin(modelRegistration: AmplifyModels())
+    do {
+        try Amplify.add(plugin: dataStorePlugin)
+        try Amplify.configure()
+        print("Initialized Amplify");
+    } catch {
+        // simplified error handling for the tutorial
+        print("Could not initialize Amplify: \(error)")
     }
 }
